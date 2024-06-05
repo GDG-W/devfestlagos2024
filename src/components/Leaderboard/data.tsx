8e029c5bdaf41ecb8ad3bacf0a1ea55d4127e8b2
Jude Tochy <tochyokoye@gmail.com>
2024-06-05 14:06:19 +0100

    reverting changes to main

--- src/components/Leaderboard/data.tsx
+++
@@ -1,3 +0,0 @@
-export const colorCodes = ["#4285F4", "#34A853", "#EFC976", "#EA4335"];
-
-
--- src/libs/config.ts
+++
@@ -1,3 +0,0 @@
-export const BACKEND_URL = "https://us-central1-devfest-lagos-2024.cloudfunctions.net/api";
-export const WEBSITE_URL = "https://dflagos24-trivia.netlify.app";
-export const LOCAL_URL = "http://localhost:5173";
