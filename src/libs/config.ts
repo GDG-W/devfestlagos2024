25d62aec3ff46d35890f98ac1cbf0325299637b1
Jude Tochy <tochyokoye@gmail.com>
2024-06-13 11:39:43 +0100

    updated url

--- src/libs/config.ts
+++ src/libs/config.ts
@@ -1,3 +1,3 @@
 export const BACKEND_URL = "https://us-central1-devfest-lagos-2024.cloudfunctions.net/api";
-export const WEBSITE_URL = "https://dflagos24-trivia.netlify.app";
+export const WEBSITE_URL = "https://www.devfestlagos.com";
 export const LOCAL_URL = "http://localhost:5173";
